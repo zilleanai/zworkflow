1f9a601a3810446cd627598fab9cddf22eddf3fe
Christian M <chriamue@gmail.com>
2019-03-16 16:17:42 +0100

    added segmentation to test

---
+++ tests/res/segmentation_dataset.py
@@ -0,0 +1,54 @@
+import os
+import numpy as np
+import cv2
+from PIL import Image
+from mlworkflow.dataset import DataSetBase
+
+
+class dataset(DataSetBase):
+
+    def __init__(self, config, preprocessing=None):
+        super().__init__(config)
+        self.features = config['dataset']['features']
+        self.labels = config['dataset']['labels']
+        self.width = config['dataset']['width']
+        self.height = config['dataset']['height']
+        self.preprocessing = preprocessing
+        self.load(config['dataset']['train_images'], config['dataset']['train_masks'])
+
+    def load(self, images='.', masks='.'):
+        self.images = sorted([os.path.join(images,f) for f in os.listdir(images)
+                             if f.endswith('.png') or f.endswith('.tif') or f.endswith('.jpg')])
+        self.masks = sorted([os.path.join(masks,f) for f in os.listdir(masks)
+                             if f.endswith('.png') or f.endswith('.tif') or f.endswith('.jpg')])
+        assert(len(self.images) == len(self.masks))
+
+    def load_image(self, path):
+        image = Image.open(path)
+        image = image.convert('RGB')
+        image = np.array(image)
+        return image
+    
+    def load_mask(self, path):
+        mask = Image.open(path)
+        mask = np.array(mask)
+        return mask
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        mask = self.masks[idx]
+
+        image = self.load_image(image)
+        mask = self.load_mask(mask)
+        (image, mask) = self.preprocessing.process((image, mask))
+
+        image = np.rollaxis(image, 2, 0)
+        image = image.astype(np.float32)
+        mask = mask.astype(np.float32)
+        return [image, mask]
+
+    def __len__(self):
+        return len(self.images)
+
+    def __str__(self):
+        return "images: " + str(self.config['dataset']['train_images']) + " masks: " + str(self.config['dataset']['train_masks']) + " len: " + str(len(self.images))
---
+++ tests/res/segmentation_model.py
@@ -0,0 +1,110 @@
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from mlworkflow.model import ModelBase
+
+# source: https://github.com/usuyama/pytorch-unet/blob/master/pytorch_unet.py
+
+
+def double_conv(in_channels, out_channels, kernel=3):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, kernel, padding=1),
+        nn.ReLU(inplace=True)
+    )
+
+
+class CNet(nn.Module):
+    # source: https://github.com/chriamue/Cnet/blob/master/Cnet/Cnet.py
+    # https://github.com/usuyama/pytorch-unet/blob/master/pytorch_unet.py
+    def __init__(self, classes):
+        super(CNet, self).__init__()
+        self.kernel = 3
+        self.activation = nn.ReLU()
+        self.batchnorm = nn.BatchNorm2d(64)
+
+        self.down1 = double_conv(3, 64, self.kernel)
+        self.down2 = double_conv(64, 128, self.kernel)
+        self.down3 = double_conv(128, 256, self.kernel)
+        self.down4 = double_conv(256, 512, self.kernel)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(
+            scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.up4 = double_conv(256+512, 256, self.kernel)
+        self.up3 = double_conv(128+256, 128, self.kernel)
+        self.up2 = double_conv(128+64, 64, self.kernel)
+        self.up1 = nn.Conv2d(64, classes, 1)
+
+    def forward(self, x):
+        conv1 = self.down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.down2(x)
+        x = self.maxpool(conv2)
+
+        conv3 = self.down3(x)
+        x = self.maxpool(conv3)
+
+        x = self.down4(x)
+
+        x = self.upsample(x)
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.up4(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv1], dim=1)
+
+        x = self.up2(x)
+        out = self.up1(x)
+        return out
+
+    # source: https://stackoverflow.com/questions/49433936/how-to-initialize-weights-in-pytorch
+
+    def init_weights(self, m):
+        if type(m) == nn.Linear:
+            torch.nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
+
+
+class model(ModelBase):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.width = config['dataset']['width']
+        self.height = config['dataset']['height']
+        self.outwidth = config['dataset']['outwidth']
+        self.outheight = config['dataset']['outheight']
+        self.classes = config['dataset']['classes']
+        self.initNet()
+
+    def initNet(self):
+        self.__net = CNet(self.classes)
+        self.__net.apply(self.__net.init_weights)
+
+    def net(self):
+        return self.__net
+
+    def __str__(self):
+        return str(self.classes)
+
+    def save(self):
+        torch.save(self.__net.state_dict(), self.config['model']['savepath'])
+        if self.config['general']['verbose']:
+            print('saved model: ', self.config['model']['savepath'])
+
+    def load(self):
+        if os.path.exists(self.config['model']['savepath']):
+            self.__net.load_state_dict(torch.load(
+                self.config['model']['savepath']))
+            if self.config['general']['verbose']:
+                print('loaded model: ', self.config['model']['savepath'])
